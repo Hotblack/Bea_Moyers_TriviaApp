adf77569cd9fbadba6f55d1a54a49926cab16b5b
Adam Jackrel <ajackrel@gmail.com>
2020-04-08 16:19:11 -0400

    first commit

---
+++ Model/QuizBrain.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  TriviaApp
+//
+//  Created by Adam Jackrel on 4/8/20.
+//  Copyright © 2020 Adam Jackrel. All rights reserved.
+//
+
+import Foundation
+
+//begin struct
+
+//array of Question structs
+
+//variable for our array index
+//variable for number of right answers
+//variable for number of wrong answers
+//variable to act as a flag if the user reaches the end of the quiz
+
+//FUNCTION - checking the users answer
+    //FORM: func function_name(user's answer (ie a String) -> returns a true or false
+    //conditional statement that will return true if user's answer matches, false if not
+
+//FUNCTION - retrieving the questions text
+    //FORM: func functions_name() -> returns question (ie String)
+
+//FUNCTION - Retrieves user's progress (question number / total number of questions)
+    //FORM: func functions_name() -> returns a number (ie Float)
+
+//FUNCTION - retrieves user's score (score right / total number of answers)
+    //FORM: func functions_name -> This could a String ("this is your score...") or could just be a number (ie Float)
+
+//FUNCTION - updates the variable that is acting as our array index (where in the index are we currently). When the user answers a question, we want to move to the next question
+    //FORM: mutating func function_name()
