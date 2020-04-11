b00df56fecda41ff320204e4d27f0a94011169ef
Adam Jackrel <ajackrel@gmail.com>
2020-04-11 19:47:57 -0400

    added some starter code and more comments

--- Model/QuizBrain.swift
+++ Model/QuizBrain.swift
@@ -6,16 +6,36 @@
 //  Copyright © 2020 Adam Jackrel. All rights reserved.
 //
 
+//this struct will be the heart of the question bank
+//it contains two fields, and no functions
+//only serving to be the "question/answer" part of the code
+struct Question {
+    
+    //two fields for qustions and answers
+    //both declared as Strings, since nothing else will be in there
+    var text: String
+    var answer: String
+    
+    //our init function will initialize the two fields with whatever
+    init(q: String, a: String) {
+        text = q;
+        answer = a;
+    }
+    
+}
+
 import Foundation
 
 //begin struct
 
 //array of Question structs
+//instead of an array of Strings (like in the Town example) we will have an array of Question structs
+//FORM: let Question(q: "question1", a: "answer1", q: "question2", a: "answer2")
 
-//variable for our array index
-//variable for number of right answers
-//variable for number of wrong answers
-//variable to act as a flag if the user reaches the end of the quiz
+//variable for our array index - Int
+//variable for number of right answers - Int
+//variable for number of wrong answers - Int
+//variable to act as a flag if the user reaches the end of the quiz - Int or Boolean or String
 
 //FUNCTION - checking the users answer
     //FORM: func function_name(user's answer (ie a String) -> returns a true or false
